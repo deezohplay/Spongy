23f1e64cbe5fcf9ecbc72ada25c43aa3c6666e83
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2024-06-02 14:07:50 +0300

    Created player movement
    created player movement left and right, and jump.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // all game variables declared
     public float speed;
+    public float jump;
     private Rigidbody2D rb;
-    private float move;
-    // Start is called before the first frame update
+    private float moveHorizontal;
+    private float moveVertical;
+    public Joystick joystick;
+    public bool isJumping;
+
+    // start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // update is called once per frame
     void Update()
     {
-        move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+        //moves the player left and right
+        if(joystick.Horizontal >= .2f)
+        {
+            moveHorizontal = speed;
+        }
+        else if(joystick.Horizontal <= -.2f)
+        {
+            moveHorizontal = -speed;
+        }
+        else
+        {
+            moveHorizontal = 0f;
+        }
+        rb.velocity = new Vector2(speed * moveHorizontal, rb.velocity.y);
+
+        //lets the player jump only if on the ground
+        if(joystick.Vertical >= .5f && isJumping == false)
+        {
+            moveVertical = jump;
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
