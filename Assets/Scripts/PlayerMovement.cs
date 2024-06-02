f0627c3cbd58b4acd6751ab1615c1e107785da1c
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2024-06-02 11:53:40 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+    private float move;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
     }
 }
