a3dab7f410188964869b65188d703f994ab84465
Rubangakene Inno Joseph <57228148+deezohplay@users.noreply.github.com>
2024-06-05 17:26:19 +0300

    game controller

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Text title;
+    public Button playButton;
+    public bool isGameOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOn = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOn = true)
+        {
+            //turn off button and title
+            //start game sound and effects
+            //turn off cool game background
+        }
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Assembly");
+        isGameOn =  true;
+    }
+}
